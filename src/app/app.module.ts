4ee35e5664dbfc6720cc54dc56d02f86063e1b22
rene <rhernandez@novasolutionsystemscom>
2018-10-02 09:27:13 -0500

    Se agregan Header y Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
