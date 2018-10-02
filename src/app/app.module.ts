79ebb417dd75ccabb5ce2bb18589fa9050fd2eb1
Rene Hernandez <bellicose@MacBook-Air-de-Rene.local>
2018-10-01 21:55:29 -0500

    Se crea clientes serivice con archivo

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  //providers: [ClienteService],
+  providers:[],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
